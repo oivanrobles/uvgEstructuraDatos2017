050fd9bab9c1e601fa37bbf4c41a4a894676611f
Oever González <notengobattery@gmail.com>
2017-02-15 12:42:02 -0600

    Bubble Sort
    - Agrega .gitignore
    - Agrega proyecto de IntelliJ
    - Agrega código de Buuble Sort en la clase Listas.Lista
    - Agrega código de prueba en la clase Sorting.NewMain

--- EstructuraDatos2017/src/Listas/Lista.java
+++ EstructuraDatos2017/src/Listas/Lista.java
@@ -5,70 +5,63 @@
  */
 package Listas;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.lang.Object;
 /**
- *
  * @author OscarIvan
  */
-public class Lista <U extends Comparable> implements Comparable{
-    
-    protected Nodo<U> Cabeza; 
-    
-    
-    public void AgregarNodoCabeza(U valor)
-    {
-        Nodo<U> NuevoNodo = new<U> Nodo(valor);
+public class Lista<U extends Comparable> implements Comparable {
+
+    protected Nodo<U> Cabeza;
+
+    public void AgregarNodoCabeza(U valor) {
+        Nodo<U> NuevoNodo = new <U>Nodo(valor);
         NuevoNodo.setSiguiente(Cabeza);
-        Cabeza = NuevoNodo;        
+        Cabeza = NuevoNodo;
     }
-    public void AgregarNodo(U valor)
-    {
+
+    public void AgregarNodo(U valor) {
         Nodo Cola = ObtenerUltimo();
         //si no existe ningun nodo, crea la cabeza, de lo contrario agrega el nuevo Nodo al siguiente de la cola
-        if ( Cola== null)
-            Cabeza = new Nodo (valor);
-        else                    
-            Cola.setSiguiente(new Nodo(valor));                                            
+        if (Cola == null) {
+            Cabeza = new Nodo(valor);
+        } else {
+            Cola.setSiguiente(new Nodo(valor));
+        }
     }
-    public Nodo ObtenerCabeza()
-    {
+
+    public Nodo ObtenerCabeza() {
         return Cabeza;
     }
-    public Nodo ObtenerUltimo()
-    {
+
+    public Nodo ObtenerUltimo() {
         Nodo NodoActual = Cabeza;
-        if (NodoActual == null)
+        if (NodoActual == null) {
             return null;
-        while (NodoActual.getSiguiente() !=  null)
-        {
+        }
+        while (NodoActual.getSiguiente() != null) {
             NodoActual = NodoActual.getSiguiente();
         }
         return NodoActual;
-        
+
     }
-    public Nodo EliminarNodo()
-    {
-        Nodo NodoADevolver = Cabeza; 
+
+    public Nodo EliminarNodo() {
+        Nodo NodoADevolver = Cabeza;
         Cabeza = Cabeza.getSiguiente();
         return NodoADevolver;
-    
+
     }
+
     @Override
-    public String toString()
-    {
+    public String toString() {
         StringBuilder sb = new StringBuilder();
-        if (Cabeza != null)
-        {
+        if (Cabeza != null) {
             Nodo NodoActual = Cabeza;
-            while (NodoActual!= null)
-            {
+            while (NodoActual != null) {
                 sb.append(NodoActual.getValor());
-                sb.append ("->");
+                sb.append("->");
                 NodoActual = NodoActual.getSiguiente();
             }
-            
+
         }
         sb.append("null");
         return sb.toString();
@@ -77,8 +70,50 @@ public class Lista <U extends Comparable> implements Comparable{
 
     @Override
     public int compareTo(Object t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException(
+                "Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    
+
+    // Pieza de código cortesía de NoTengoBattery
+    public void bubbleSort() {
+        boolean wasChanged = true;
+        // wasChanged es True durante el cuerpo de este while, después de las condiciones iniciales
+        // si la lista ha sido modificada. Se sabe que la lista se modifica solo si no está ordenada
+        while (wasChanged) {
+            // 4 condiciones iniciales del estado conocido
+            wasChanged = false;
+            Nodo l_anterior = null;
+            Nodo l_actual = Cabeza;
+            Nodo l_siguiente = l_actual.getSiguiente();
+            // Este While recorre toda la lista
+            while (l_siguiente != null) {
+                final int l_result = l_actual.compareTo(l_siguiente.getValor());
+                if (l_result >= 1) {
+                    Nodo l_tercero = l_siguiente.getSiguiente();
+                    // El primer nodo apunta al tercer nodo
+                    l_actual.setSiguiente(l_tercero);
+                    // El segundo nodo apunta al primer nodo
+                    l_siguiente.setSiguiente(l_actual);
+                    // El nodo anterior apunta al nodo siguiente si no es null
+                    if (l_anterior != null) {
+                        l_anterior.setSiguiente(l_siguiente);
+                    }
+                    // Indica que la lista ha cambiado
+                    wasChanged = true;
+                    // Cambia la cabeza si el nodo ordenado es la cabeza
+                    Cabeza = ((l_actual == Cabeza) ? l_siguiente : Cabeza);
+                    // "Actual" y "siguiente" están en orden inverso, actual es mayor
+                    final Nodo l_intercambio = l_actual;
+                    // Intercambia el actual con el siguiente (solamente los punteros)
+                    l_actual = l_siguiente;
+                    l_siguiente = l_intercambio;
+                }// else:  No se mueven los nodos, ni los punteros
+                // Estas tres asignaciones mueven los punteros a los 3 nodos siguientes
+                l_anterior = l_actual;
+                l_actual = l_anterior.getSiguiente();
+                l_siguiente = l_actual.getSiguiente();
+            }
+        }
+    }
+
 }
--- EstructuraDatos2017/src/Sorting/NewMain.java
+++ EstructuraDatos2017/src/Sorting/NewMain.java
@@ -4,11 +4,11 @@
  * and open the template in the editor.
  */
 package Sorting;
-import Listas.*;
+
 import EjercicioRadio.MemoryBoton;
+import Listas.Lista;
 
 /**
- *
  * @author OscarIvan
  */
 public class NewMain {
@@ -17,53 +17,61 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
-        
-        
+
         // TODO code application logic here
-        String [] texto = new String[5];
-        texto[0]= "hello";
-        texto[1]= "from";
-        texto[2]= "the ";
-        texto[3]= "other";
-        texto[4]= "side";       
-        
+        String[] texto = new String[5];
+        texto[0] = "hello";
+        texto[1] = "from";
+        texto[2] = "the ";
+        texto[3] = "other";
+        texto[4] = "side";
+
         Sorting.selectionSort(texto);
-        for (int i =0; i< 5; i++)
-        {
-            System.out.println(texto [i]);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(texto[i]);
         }
-                
-                
+
         Contact[] Agenda = new Contact[5];
-        Agenda[0] = new Contact("Juan", "Martinez","12345678");
-        Agenda[1] = new Contact("Pedro", "Gomez","12345678");
-        Agenda[2] = new Contact("Esteban", "Gonzalez","12345678");
-        Agenda[3] = new Contact("Maria", "Morazan","12345678");
-        Agenda[4] = new Contact("Carla", "Solis","12345678");
-        
+        Agenda[0] = new Contact("Juan", "Martinez", "12345678");
+        Agenda[1] = new Contact("Pedro", "Gomez", "12345678");
+        Agenda[2] = new Contact("Esteban", "Gonzalez", "12345678");
+        Agenda[3] = new Contact("Maria", "Morazan", "12345678");
+        Agenda[4] = new Contact("Carla", "Solis", "12345678");
+
+        Lista<String> cosas = new Lista<>();
+
         Sorting.selectionSort(Agenda);
-        
-        for (int i =0; i< 5; i++)
-        {
+
+        for (int i = 0; i < 5; i++) {
             System.out.println(Agenda[i]);
         }
-        
+
         MemoryBoton[] ABotones = new MemoryBoton[5];
-        
-        ABotones[0] =(new MemoryBoton("AM","530"));
-        ABotones[1] =(new MemoryBoton("FM","96.5"));
-        ABotones[2] =(new MemoryBoton("AM","540"));
-        ABotones[3] =(new MemoryBoton("AM","560"));
-        ABotones[4] =(new MemoryBoton("FM","94.9"));
-        
+
+        ABotones[0] = (new MemoryBoton("AM", "530"));
+        ABotones[1] = (new MemoryBoton("FM", "96.5"));
+        ABotones[2] = (new MemoryBoton("AM", "540"));
+        ABotones[3] = (new MemoryBoton("AM", "560"));
+        ABotones[4] = (new MemoryBoton("FM", "94.9"));
+
         Sorting.selectionSort(ABotones);
-        for (int i =0; i< 5; i++)
-        {
-            System.out.println(ABotones [i]);
-        }        
-            
+        for (int i = 0; i < 5; i++) {
+            System.out.println(ABotones[i]);
+        }
+
+        cosas.AgregarNodo("Hola");
+        cosas.AgregarNodo("Alo");
+        cosas.AgregarNodo("Non");
+        cosas.AgregarNodo("Yuni");
+        cosas.AgregarNodo("Anda");
+        cosas.AgregarNodo("Dony");
+
+        System.out.print("\nLista sin ordenar:\n\t");
+        System.out.print(cosas);
+        cosas.bubbleSort();
+        System.out.print("\nLista ordenada:\n\t");
+        System.out.print(cosas);
+
     }
-    
+
 }
