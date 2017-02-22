73d3b6a349bcf132d19a84d89b69023484a32071
Oever González <notengobattery@gmail.com>
2017-02-22 11:45:49 -0600

    Implementada la fábrica

--- EstructuraDatos2017/src/com/gmail/notengobattery/fabrica/FigurasFabrica.java
+++ EstructuraDatos2017/src/com/gmail/notengobattery/fabrica/FigurasFabrica.java
@@ -4,4 +4,16 @@ package com.gmail.notengobattery.fabrica;
  * Esta es una fábrica de figuras
  */
 public class FigurasFabrica {
+    Figura getFigura(final int lados, final double... medidas) {
+        switch (lados) {
+            case 0:
+                return new Circulo(medidas[0]);
+            case 4:
+                return new Cuadrado(medidas[0]);
+            case -4:
+                return new Rectangulo(medidas[0], medidas[1]);
+            default:
+                return null;
+        }
+    }
 }
