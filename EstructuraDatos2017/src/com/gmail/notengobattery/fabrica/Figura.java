2c82fc441d21ce05ce81b53d2096b284d31b3b7c
Oever González <notengobattery@gmail.com>
2017-02-22 11:28:07 -0600

    Establece la interfaz Figura

--- EstructuraDatos2017/src/com/gmail/notengobattery/fabrica/Figura.java
+++ EstructuraDatos2017/src/com/gmail/notengobattery/fabrica/Figura.java
@@ -4,4 +4,23 @@ package com.gmail.notengobattery.fabrica;
  * Esta interfaz se utiliza para generar figuras genéricas
  */
 public interface Figura {
+    /**
+     * Dibuja la figura.
+     */
+    void dibujar();
+
+    /**
+     * @return el área de la figura
+     */
+    double getArea();
+
+    /**
+     * @return el perímetro de la figura
+     */
+    double getPerimetro();
+
+    /**
+     * @return el número de lados de la figura
+     */
+    double getLados();
 }
